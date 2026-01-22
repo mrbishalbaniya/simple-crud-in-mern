0c4d53b8a986ad54c57c834b0797973a7ed5a960
Bishal Baniya <mr.bishal.baniya@gmail.com>
2026-01-22 20:16:50 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route");
+const logger = require("./middleware/logger.middleware");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(logger); 
+app.use(express.static('public'));
+
+// Routes  
+app.use("/api/products", productRoute);
+
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI)
+    .then(() => console.log("Connected to DB!"))
+    .catch((err) => console.log("Error:", err));
+
+app.get("/", (req, res) => res.send("Welcome to the API"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
