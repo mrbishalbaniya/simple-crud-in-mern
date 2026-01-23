3ac9ed7794e0c66838bb138b522ffcda65898b71
Bishal Baniya <mr.bishal.baniya@gmail.com>
2026-01-23 19:50:43 +0545

    hashing added

--- models/user.model.js
+++ models/user.model.js
@@ -8,11 +8,18 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre('save', async function () {
+    // 1. Only hash if the password is new or being changed
+    if (!this.isModified('password')) return; 
+
+    try {
+        // 2. Generate salt and hash
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        // Note: No next() here! Mongoose handles it via the async return.
+    } catch (err) {
+        throw new Error(err); // Mongoose will catch this as a validation error
+    }
 });
 
 // Helper method to compare passwords
